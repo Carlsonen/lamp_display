5e4a274b2d56753184044b318dd8b0e621bc535a
Oscar <ocarlsson2002@gmail.com>
2022-11-20 15:36:20 +0100

    fixed shit

--- src/lib.rs
+++ src/lib.rs
@@ -10,35 +10,33 @@ impl RedstoneDisplay {
         let lamp_texture_on = Limage::open(&"texture/lamp_on.png".to_string())?;
         let lamp_texture_off = Limage::open(&"texture/lamp_off.png".to_string())?;
 
-        let mut img = Limage::new(32 * width, 32 * height);
+        let mut img = Limage::new(2 * 16 * width, 2 * 16 * height);
         for y in 0..height {
             for x in 0..width {
-                img.paste((32 * x as i32, 32 * y as i32), &lamp_texture_off);
-                img.paste((32 * x as i32 + 16, 32 * y as i32), &lamp_texture_off);
-                img.paste((32 * x as i32, 32 * y as i32 + 16), &lamp_texture_off);
-                img.paste((32 * x as i32 + 16, 32 * y as i32 + 16), &lamp_texture_off);
+                paste4(x, y, &mut img, &lamp_texture_off);
             }
         }
 
         Ok(RedstoneDisplay { 
-            img: img, 
-            lamp_texture_on: lamp_texture_on, 
-            lamp_texture_off: lamp_texture_off 
+            img, 
+            lamp_texture_on, 
+            lamp_texture_off
         })
     }
     pub fn save(&self, path: &str) {
         self.img.save(path).unwrap()
     }
     pub fn turn_lamp_on(&mut self, x: u32, y: u32) {
-        self.img.paste((32 * x as i32, 32 * y as i32), &self.lamp_texture_on);
-        self.img.paste((32 * x as i32 + 16, 32 * y as i32), &self.lamp_texture_on);
-        self.img.paste((32 * x as i32, 32 * y as i32 + 16), &self.lamp_texture_on);
-        self.img.paste((32 * x as i32 + 16, 32 * y as i32 + 16), &self.lamp_texture_on);
+        paste4(x, y, &mut self.img, &self.lamp_texture_on);
     }
     pub fn turn_lamp_off(&mut self, x: u32, y: u32) {
-        self.img.paste((32 * x as i32, 16 * y as i32), &self.lamp_texture_off);
-        self.img.paste((32 * x as i32 + 16, 16 * y as i32), &self.lamp_texture_off);
-        self.img.paste((32 * x as i32, 16 * y as i32 + 16), &self.lamp_texture_off);
-        self.img.paste((32 * x as i32 + 16, 16 * y as i32 + 16), &self.lamp_texture_off);
+        paste4(x, y, &mut self.img, &self.lamp_texture_off);
     }
+}
+
+fn paste4(x: u32, y: u32, img: &mut Limage, texture: &Limage) {
+    img.paste((32 * x as i32,      32 * y as i32),      texture);
+    img.paste((32 * x as i32 + 16, 32 * y as i32),      texture);
+    img.paste((32 * x as i32,      32 * y as i32 + 16), texture);
+    img.paste((32 * x as i32 + 16, 32 * y as i32 + 16), texture);
 }
