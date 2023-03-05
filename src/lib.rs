b5ef048fc6fa2b44519c22724b03c52dbe982706
Carlsonen <ocarlsson2002@gmail.com>
2023-03-05 20:40:15 +0100

    update to new limage api

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use limage::*;
-
 pub trait Display {
     type Disp;
 
@@ -9,28 +8,28 @@ pub trait Display {
     fn turn_lamp_off(&mut self, x: u32, y: u32);
 }
 pub struct RedstoneDisplay {
-    img: Limage,
-    lamp_texture_on: Limage,
-    lamp_texture_off: Limage
+    img: LimageRgb,
+    lamp_texture_on: LimageRgb,
+    lamp_texture_off: LimageRgb,
 }
 impl Display for RedstoneDisplay {
     type Disp = RedstoneDisplay;
 
     fn new(width: u32, height: u32) -> Result<Self, String> {
-        let lamp_texture_on = Limage::open(&"texture/lamp_on.png".to_string())?;
-        let lamp_texture_off = Limage::open(&"texture/lamp_off.png".to_string())?;
+        let lamp_texture_on = LimageRgb::open(&"texture/lamp_on.png".to_string())?;
+        let lamp_texture_off = LimageRgb::open(&"texture/lamp_off.png".to_string())?;
 
-        let mut img = Limage::new(2 * 16 * width, 2 * 16 * height);
+        let mut img = LimageRgb::new(2 * 16 * width, 2 * 16 * height);
         for y in 0..height {
             for x in 0..width {
                 paste4(x, y, &mut img, &lamp_texture_off);
             }
         }
 
-        Ok(RedstoneDisplay { 
-            img, 
-            lamp_texture_on, 
-            lamp_texture_off
+        Ok(RedstoneDisplay {
+            img,
+            lamp_texture_on,
+            lamp_texture_off,
         })
     }
     fn save(&self, path: &str) {
@@ -45,13 +44,13 @@ impl Display for RedstoneDisplay {
 }
 
 pub struct PixelDisplay {
-    img: Limage
+    img: LimageRgb,
 }
 impl Display for PixelDisplay {
     type Disp = PixelDisplay;
 
     fn new(width: u32, height: u32) -> Result<Self, String> {
-        let img = Limage::new(width, height);
+        let img = LimageRgb::new(width, height);
         Ok(PixelDisplay { img })
     }
     fn save(&self, path: &str) {
@@ -65,9 +64,9 @@ impl Display for PixelDisplay {
     }
 }
 
-fn paste4(x: u32, y: u32, img: &mut Limage, texture: &Limage) {
-    img.paste((32 * x as i32,      32 * y as i32),      texture);
-    img.paste((32 * x as i32 + 16, 32 * y as i32),      texture);
-    img.paste((32 * x as i32,      32 * y as i32 + 16), texture);
+fn paste4(x: u32, y: u32, img: &mut LimageRgb, texture: &LimageRgb) {
+    img.paste((32 * x as i32, 32 * y as i32), texture);
+    img.paste((32 * x as i32 + 16, 32 * y as i32), texture);
+    img.paste((32 * x as i32, 32 * y as i32 + 16), texture);
     img.paste((32 * x as i32 + 16, 32 * y as i32 + 16), texture);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-
 use lamp_display::*;
-
 fn main() {
     let mut display = RedstoneDisplay::new(64, 64).unwrap();
 }
